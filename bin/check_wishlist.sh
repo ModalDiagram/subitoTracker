cf2a338aae2c77e32b80869c84c746465ac0128b
ModalDiagram <stodgy_goilg@aleeas.com>
2023-06-28 11:15:54 +0200

    .

--- bin/check_wishlist.sh
+++ bin/check_wishlist.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+dir_path="$(dirname "$0")/.."
+
 while read -r token; do
   read -r chatid
   break;
-done < "$HOME/PycharmProjects/subitoTracker/bin/secrets.txt"
+done < "$dir_path/bin/secrets.txt"
 wget -q --tries=10 --timeout=20 --spider http://google.com
 if [[ $? -eq 0 ]]; then
   read -r timestamp < $HOME/.cache/search_offer/last_check.txt
   while read -r line; do
     nome=${line#*:}
     range=${line%:*}
-    $HOME/PycharmProjects/subitoTracker/src/search_offer.py -p $range -i -a $token $chatid -t $timestamp $nome
-  done < "$HOME/PycharmProjects/subitoTracker/wishlist.txt"
+    $dir_path/src/search_offer.py -p $range -i -a $token $chatid -t $timestamp $nome
+  done < "$dir_path/wishlist.txt"
 
   echo $(date +"%s") > $HOME/.cache/search_offer/last_check.txt
 fi
